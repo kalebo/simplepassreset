6cfbe67acfa84294d654bb2d8b17784a4ffba7ac
Kaleb Olson <kaleb.olson@gmail.com>
2017-09-07 12:15:14 -0600

    Reformated code

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"sync"
-	"math/rand"
 	"time"
-	"fmt"
 
 	"github.com/go-zoo/bone"
 )
 
 const (
-	PORT string = ":4555"
-	TTL time.Duration = time.Duration(time.Hour * 5)
+	PORT string        = ":4555"
+	TTL  time.Duration = time.Duration(time.Hour * 5)
 )
 
 type resetRequest struct {
 	accountname string
-	created time.Time 
+	created     time.Time
 }
 
 func (r *resetRequest) expired() bool {
 	return time.Now().Sub(r.created) > TTL
 }
 
-var resetRequestsMap = struct{
+var resetRequestsMap = struct {
 	sync.RWMutex
 	m map[string]resetRequest // i.e, base64 secret maps to username
 }{m: make(map[string]resetRequest)}
@@ -45,7 +45,7 @@ func main() {
 }
 
 func tidyRequestsMap() {
-	resetRequestsMap.Lock() // Guards are wider than I like so I need to check 
+	resetRequestsMap.Lock() // Guards are wider than I like so I need to check
 	//that golang iteration can handle the map changing while iterating
 	for k, v := range resetRequestsMap.m {
 		if v.expired() {
@@ -54,13 +54,13 @@ func tidyRequestsMap() {
 	}
 	resetRequestsMap.Unlock()
 
-	time.AfterFunc(time.Minute * 5, tidyRequestsMap)
+	time.AfterFunc(time.Minute*5, tidyRequestsMap)
 }
 
 func cliInterface() {
 	for true {
 		fmt.Print("Generate reset link for: ")
-		var input string 
+		var input string
 		fmt.Scanln(&input)
 		token := RandString(64)
 
@@ -79,48 +79,48 @@ func resetForm(rw http.ResponseWriter, r *http.Request) {
 	resetRequestsMap.Lock()
 	defer resetRequestsMap.Unlock()
 	if val, ok := resetRequestsMap.m[token]; ok {
-		// return the form 
-	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
-	rw.Write([]byte(fmt.Sprintf(`<html>
-	<head>
-	<title>Password Reset</title>
-	</head>
-	<body>
-	<form class="pure-form" method="POST">
-    <fieldset>
-        <legend>Reset password for '%s'</legend>
+		// return the form
+		rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+		rw.Write([]byte(fmt.Sprintf(`
+<html>
+<head>
+<title>Password Reset</title>
+</head>
+<body>
+<form class="pure-form" method="POST">
+<fieldset>
+	<legend>Reset password for '%s'</legend>
 
-        <input type="password" placeholder="Password" id="password" required>
-        <input type="password" placeholder="Confirm Password" id="confirm_password" required>
+	<input type="password" placeholder="Password" id="password" required>
+	<input type="password" placeholder="Confirm Password" id="confirm_password" required>
 
-        <button type="submit" class="pure-button pure-button-primary">Confirm</button>
-		</br>
-		</br>
-		<small style="color:grey"> This form will expire on %s </small>
-    </fieldset>
-	</form>
-	<script>
-		var password = document.getElementById("password")
-		, confirm_password = document.getElementById("confirm_password");
+	<button type="submit" class="pure-button pure-button-primary">Confirm</button>
+	</br>
+	</br>
+	<small style="color:grey"> This form will expire on %s </small>
+</fieldset>
+</form>
+<script>
+	var password = document.getElementById("password")
+	, confirm_password = document.getElementById("confirm_password");
 
-		function validatePassword(){
-		if(password.value != confirm_password.value) {
-			confirm_password.setCustomValidity("Passwords Don't Match");
-		} else {
-			confirm_password.setCustomValidity('');
-		}
-		}
+	function validatePassword(){
+	if(password.value != confirm_password.value) {
+		confirm_password.setCustomValidity("Passwords Don't Match");
+	} else {
+		confirm_password.setCustomValidity('');
+	}
+	}
 
-		password.onchange = validatePassword;
-		confirm_password.onkeyup = validatePassword;
-	</script>
-	</body>
-	</html>`, val.accountname, time.Now().Add(TTL).Format("2006-01-02T15:04:05 MST") )))
-	return
-
+	password.onchange = validatePassword;
+	confirm_password.onkeyup = validatePassword;
+</script>
+</body>
+</html>`, val.accountname, time.Now().Add(TTL).Format("2006-01-02T15:04:05 MST"))))
+		return
 
 	}
-	
+
 	http.NotFound(rw, r)
 
 }
@@ -134,14 +134,15 @@ func resetPassword(rw http.ResponseWriter, r *http.Request) {
 	if _, ok := resetRequestsMap.m[token]; ok {
 		// reset the password by calling the api async
 		rw.Header().Set("Content-Type", "text/html; charset=utf-8")
-		rw.Write([]byte(`<html>
-		<head>
-		<title>Password Reset</title>
-		</head>
-		<body>
-		Password has been reset!
-		</body>
-		</html>
+		rw.Write([]byte(`
+<html>
+<head>
+<title>Password Reset</title>
+</head>
+<body>
+Password has been reset!
+</body>
+</html>
 		`))
 
 		delete(resetRequestsMap.m, token)
@@ -155,9 +156,9 @@ func resetPassword(rw http.ResponseWriter, r *http.Request) {
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func RandString(n int) string {
-    b := make([]byte, n)
-    for i := range b {
-        b[i] = letterBytes[rand.Intn(len(letterBytes))]
-    }
-    return string(b)
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
